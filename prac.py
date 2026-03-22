274b50ac3a937fb14b115847006452b7c0f3d0b4
Hephaestus0405 <rahul405.bhardwaj@gmail.com>
2026-03-22 13:34:28 +0530

    new_commit

--- prac.py
+++ prac.py
@@ -1,10 +1,16 @@
 def sum(l):
     out=0
-    for i in l:
-        out+=i
-    return out
+    # for i in l:
+    #     out+=i
+    # return out
 
 out=sum([3,4,5,6])
 print(out)
 
 ### NEW CHANGES#####
+
+def mul():
+    pass
+
+
+mul()
